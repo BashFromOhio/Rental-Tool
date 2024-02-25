3d8e20cdfdcf7269b1c18dbaceade48a41ae80bf
Sebastian Holt <SHolt@Sebastian-Holts-MacBook-Pro.local>
2024-02-25 14:27:25 -0500

    update setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rental-tool",
+    version="0.0.0",
+    author="Bash",
+    author_email="bashholt@gmail.com",
+    packages=find_packages()
+)
